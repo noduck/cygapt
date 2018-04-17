493577ba54f89f7426e8b80b2bb4912dd630f28d
Raf D <raf@noduck.org>
2018-04-17 12:57:14 -0400

    bootstrap.sh finds cygapt dependencies, downloads and installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,124 @@
 #!/bin/bash
 
+exec 4>&1
+
 function get {
 	url="$1"
-	method="${url%%:*}"
-	uri="${url##$method://}"
+	proto="${url%%:*}"
+	uri="${url##$proto://}"
 	host="${uri%%/*}"
 	uri="/${uri#*/}"
 
-	get_http $host $uri
+	case "$proto" in
+	http)
+		echo >&4 "Get $url"
+		get_http $host $uri
+		;;
+	https)
+		echo >&4 "Get $url"
+		get_https $host $uri
+		;;
+	*)
+		echo >&2 "Unknown protocol $proto in URL $url"
+		exit 10;;
+	esac
 }
 
 function get_http {
-	exec 5<> /dev/tcp/$host/80
-	echo -ne "GET $uri HTTP/1.1\r\nHost: $host\r\nConnection: close\r\n\r\n" >&5 &
-	cat <&5
-# grep -vanm1 [:/] <file>
-# tail +<n+1> <file>
+	exec 5<> /dev/tcp/$1/80
+	echo -ne "GET $2 HTTP/1.1\r\nHost: $1\r\nConnection: close\r\n\r\n" >&5
+	# read until end of headers
+	while read header; do
+		if [ "$header" = $'\r' ]; then
+			# output the result
+			cat <&5
+			break
+		fi
+	done <&5
 }
 
 function get_https {
-	echo
+	echo -ne "GET $uri HTTP/1.1\r\nHost: $1\r\nConnection: close\r\n\r\n" |
+	 openssl s_client -quiet -connect $1:443 2>/dev/null |
+	 # read until end of headers
+	 while read header; do
+		if [ "$header" = $'\r' ]; then
+			# output the result
+			cat
+			break
+		fi
+	done
 }
 
-get http://cygwin.mirror.constant.com/x86_64/setup.bz2
+if ./cygapt -h >/dev/null 2>&1; then
+	echo "cygapt is already functional"
+	exit
+fi
+
+echo "Downloading missing cygapt dependencies"
+mirror=$(grep -A1 last-mirror /etc/setup/setup.rc)
+mirror="${mirror##last-mirror}"
+mirror="${mirror#"${mirror%%[![:space:]]*}"}"
+
+arch=$(uname -m)
+arch="${arch/i6/x}"
+
+get $mirror$arch/setup.bz2 | bzcat | while read line; do
+	case "$line" in
+	@*)
+		pack="${line##@ }"
+		release="cur"
+		continue
+		;;
+	\[*\])
+		release="${line##\[}"
+		release="${release%%\]}"
+		continue
+		;;
+	install*)
+		test "$release" = "cur" || continue
+		case "$pack" in
+		perl) ;;
+		perl_base) ;;
+		perl_autorebase) ;;
+		perl-Digest-SHA) ;;
+		perl-Scalar-List-Utils) ;;
+		perl-TermReadKey) ;;
+		perl-libwww-perl) ;;
+		# TODO: libwww-perl depends
+		perl-Encode-Locale) ;;
+		perl-File-Listing) ;;
+		perl-HTTP-Date) ;;
+		perl-HTML-Parser) ;;
+		perl-HTML-Tagset) ;;
+		perl-HTTP-Cookies) ;;
+		perl-HTTP-Message) ;;
+		perl-IO-HTML) ;;
+		perl-LWP-MediaTypes) ;;
+		perl-URI) ;;
+		perl-HTTP-Daemon) ;;
+		perl-HTTP-Negotiate) ;;
+		perl-Net-HTTP) ;;
+		perl-Try-Tiny) ;;
+		perl-WWW-RobotRules) ;;
+		*)
+			continue;;
+		esac
+		path="${line##install: }"
+		path="${path%% *}"
+		;;
+	*)
+		continue
+	esac
+
+	if cygcheck -c $pack | grep -q OK; then
+		# already installed
+		continue
+	fi
+
+	echo "Get $mirror$path"
+	get $mirror$path | tar -J -xf - -C /
+done
+
+./cygapt update
+./cygapt fix
